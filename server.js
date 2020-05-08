044c845e63e92dd6e4771e88be8b3a85ef510619
tom2u <57157539+tom2u@users.noreply.github.com>
2020-05-08 14:40:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const db = require("./db/database.js");
-const apiRoutes = require("./routes/apiRoutes");
+const express = require('express');
+const db = require('./db/database');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-const urlencodedParser = express.urlencoded({ extended: false });
-const jsonParser = express.json();
 
-app.use("/api", apiRoutes);
-
+const apiRoutes = require('./routes/apiRoutes');
 // Express middleware
-app.use(urlencodedParser);
-app.use(jsonParser);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// Default response for any other request(Not Found) Catch all
+// Use apiRoutes
+app.use('/api', apiRoutes);
+
+// Default response for any other request(Not Found) Catch all other
 app.use((req, res) => {
   res.status(404).end();
 });
 
 // Start server after DB connection
-db.on("open", () => {
+db.on('open', () => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
